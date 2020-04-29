bd7b7aa1ce4b88127497b586cdcb8abd45e7535b
iiffigues <iffigues@vivaldi.net>
2020-04-29 14:42:08 +0200

    testing tool

--- swapper/start.c
+++ swapper/start.c
@@ -1,5 +1,39 @@
 #include "ft_swap.h"
 
+int get_max(t_pile *a) {
+    int i;
+    t_pile *tmp;
+
+    if (a) {
+        i = a->i;
+        tmp = a->next;
+        while (tmp != a) {
+            if (tmp->i > i) {
+                i = tmp->i;
+            }
+            tmp = tmp->next;
+        }
+    }
+    return i;
+} 
+
+int get_min(t_pile *a) {
+    int i;
+    t_pile *tmp;
+
+    if (a) {
+        i = a->i;
+        tmp = a->next;
+        while (tmp != a) {
+            if (tmp->i < i) {
+                i = tmp->i;
+            }
+            tmp = tmp->next;
+        }
+    }
+    return i;
+}
+
 int start(t_piles *a) {
     return free_pile(a);
 }
