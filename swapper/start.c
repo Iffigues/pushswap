00540b5598c1490b7e4ba7ec9dd94fc079f26a8a
iiffigues <iffigues@vivaldi.net>
2020-04-30 17:34:08 +0200

    testing tool

--- swapper/start.c
+++ swapper/start.c
@@ -1,31 +1,19 @@
 #include "ft_swap.h"
 
-int get_max(t_pile *a) {
+int get_max(t_pile *a)
+{
     int i;
     t_pile *tmp;
 
-    if (a) {
+    i = 0;
+    if (a)
+    {
         i = a->i;
         tmp = a->next;
-        while (tmp != a) {
-            if (tmp->i > i) {
-                i = tmp->i;
-            }
-            tmp = tmp->next;
-        }
-    }
-    return i;
-} 
-
-int get_min(t_pile *a) {
-    int i;
-    t_pile *tmp;
-
-    if (a) {
-        i = a->i;
-        tmp = a->next;
-        while (tmp != a) {
-            if (tmp->i < i) {
+        while (tmp != a)
+        {
+            if (tmp->i > i)
+            {
                 i = tmp->i;
             }
             tmp = tmp->next;
@@ -34,7 +22,30 @@ int get_min(t_pile *a) {
     return i;
 }
 
-int count_list(t_pile *a) {
+int get_min(t_pile *a)
+{
+    int i;
+    t_pile *tmp;
+
+    i = 0;
+    if (a)
+    {
+        i = a->i;
+        tmp = a->next;
+        while (tmp != a)
+        {
+            if (tmp->i < i)
+            {
+                i = tmp->i;
+            }
+            tmp = tmp->next;
+        }
+    }
+    return i;
+}
+
+int count_list(t_pile *a)
+{
     t_pile *tmp;
     int i;
 
@@ -42,37 +53,15 @@ int count_list(t_pile *a) {
         return 0;
     i = 1;
     tmp = a;
-    while (tmp->next != a) {
+    while (tmp->next != a)
+    {
         i++;
         tmp = tmp->next;
     }
     return i;
 }
 
-float moyenne(t_pile *a, float min, float max) {
-    float middle;
-    t_pile *tmp;
-    int i;
-
-    i = count_list(a);
-    tmp = a;
-    middle = ((float)a->i - min)/(min - max);
-    tmp = tmp->next;
-    while (tmp != a) {
-        middle = middle + (((float)tmp->i - min)/(min - max));
-    }
-    return middle / (float)i;
-}
-
-float get_moyenne(t_pile *a) {
-    float min;
-    float max;
-
-    min = get_min(a);
-    max = get_max(a);
-    return moyenne(a, min, max);
-}
-
-int start(t_piles *a) {
+int start(t_piles *a)
+{
     return free_pile(a);
 }
